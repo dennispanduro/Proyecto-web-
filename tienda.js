041b97b1a95bfa1a370443bcb78d07bc50e0f9c4
Dennis30 <72525447@certus.edu.pe>
2025-06-23 22:59:46 -0500

    Avance12

---
+++ tienda.js
@@ -0,0 +1,57 @@
+// Array de productos con categorías
+const productos = [
+    {
+        nombre: 'Polera Blue Star',
+        categoria: 'ropa',
+        imagen: 'Producto-mascotas/perrito (2).png',
+        botonTexto: 'COMPRAR'
+    },
+    {
+        nombre: 'Chaleco contra la ansiedad',
+        categoria: 'accesorios',
+        imagen: 'Producto-mascotas/perrito (3).png',
+        botonTexto: 'COMPRAR'
+    },
+    {
+        nombre: 'Vestido Reuna Tulipán',
+        categoria: 'ropa',
+        imagen: 'Producto-mascotas/perrito (4).png',
+        botonTexto: 'COMPRAR'
+    },
+    {
+        nombre: 'Camisa Beige Sunser',
+        categoria: 'ropa',
+        imagen: 'Producto-mascotas/perrito (5).png',
+        botonTexto: 'COMPRAR'
+    },
+    {
+        nombre: 'Camisa Sugar Melón',
+        categoria: 'ropa',
+        imagen: 'Producto-mascotas/perrito (6).png',
+        botonTexto: 'COMPRAR'
+    },
+    {
+        nombre: 'Camisa Red Poppy',
+        categoria: 'ropa',
+        imagen: 'Producto-mascotas/perrito (7).png',
+        botonTexto: 'COMPRAR'
+    },
+    {
+        nombre: 'Poleta dog Pride',
+        categoria: 'ropa',
+        imagen: 'Producto-mascotas/perrito (8).png',
+        botonTexto: 'COMPRAR'
+    },
+    {
+        nombre: 'Love is love',
+        categoria: 'accesorios',
+        imagen: 'Producto-mascotas/perrito (9).png',
+        botonTexto: 'COMPRAR'
+    },
+    {
+        nombre: 'Polera Pride Day',
+        categoria: 'ropa',
+        imagen: 'Producto-mascotas/perrito (1).png',
+        botonTexto: 'COMPRAR'
+    }
+];
